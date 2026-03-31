874b938c0e5708fa35e1341cfcf4eea33d331239
Gaurav Bisht <bisht.gaurav95@gmail.com>
2026-03-31 20:25:34 +0530

    save user in database (backend folder)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const User = require('../models/User')
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-
-    obj = {
-        a: "this",
-        number : 95
-    }
-
-    res.json(obj)
+//Create User Using : Post "/api/auth"
+router.post('/', (req, res)=>{
+   console.log(req.body);
+   const user = User(req.body);
+   user.save();
+   res.send(req.body);
 })
 
 module.exports = router
