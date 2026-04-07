dc0b8b770c51c3b96de1480a3d10ed4f02a30d76
bish95 <bishgaurav95@gmail.com>
2026-04-07 16:43:35 +0530

    add validation on backend auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const User = require('../models/User')
 const router = express.Router();
+const { body, validationResult } = require('express-validator')
 
 //Create User Using : Post "/api/auth"
-router.post('/', (req, res)=>{
-   console.log(req.body);
-   const user = User(req.body);
-   user.save();
-   res.send(req.body);
+router.post('/',[
+   body('name').isLength({ min: 3 }),
+   body('email').isEmail(),
+], (req, res)=>{
+  
+   const errors = validationResult(req);
+   if(!errors.isEmpty()){
+      return res.status(400).json({ error: errors.array()})
+   }
+   // const user = User(req.body);
+   // console.log(req.body);
+   // user.save();
+
+   User.create({
+      name: req.body.name,
+      email: req.body.email,
+      //password: req.body.password
+   }).then(user => res.json(user))
+   .catch(err => {
+      console.log(err)
+      res.json({errors: 'Please enter a unique value for email'})
+   })
+
 })
 
 module.exports = router
