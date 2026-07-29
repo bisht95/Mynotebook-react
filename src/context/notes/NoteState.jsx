8bd0faee37b7bb531c9dd88d2b077ccff1961414
bish95 <bishgaurav95@gmail.com>
2026-07-29 17:59:55 +0530

    install npm cors form express  js to fix cors issuse in backend and fatch user post data from api frontend

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -1,36 +1,34 @@
 import React, { useState } from "react";
 import noteContext from "./noteContext";
 
-const NoteState = (props) =>{
-
-    const notesInitial = [
-        {
-            "_id": "6a1d664f1a86e57506d2e7e9",
-            "user": "6a1d62251a86e57506d2e7e4",
-            "title": "saurav note 2",
-            "description": "This is my second Note update",
-            "tag": "saurav personal",
-            "Date": "2026-06-01T11:00:31.346Z",
-            "__v": 0
-        },
-        {
-            "_id": "6a1d6c4075780a3b1ad9157b",
-            "user": "6a1d62251a86e57506d2e7e4",
-            "title": "saurav note 3",
-            "description": "This is my Third Note",
-            "tag": "saurav personal",
-            "Date": "2026-06-01T11:25:52.321Z",
-            "__v": 0
-        }
-    ];
-
+const NoteState = (props) => {
+    const host = "http://localhost:5000";
+    const notesInitial = [];
     const [notes, setNotes] = useState(notesInitial)
+    // Get all Notes
+
+    const getNotes = async () => {
+        //Fetch Note : Api Call
+
+        const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNmExZDYyMjUxYTg2ZTU3NTA2ZDJlN2U0In0sImlhdCI6MTc4MDMxMDc2Nn0.1WTvVopF6B45iB-1mZbBDqSQBms5xtPONeMK9InCfoQ'
+            }
+        });
+
+        const json = await response.json();
+        console.log(json)
+        setNotes(json);
+    }
+
 
     // Add a Notes
     const addNote = (title, description, tag) => {
         //TODO: API Call
         console.log("Adding a new note")
-        const note =  {
+        const note = {
             "_id": "6a1d664f1a86e57506d2e7e9",
             "user": "6a1d62251a86e57506d2e7e4",
             "title": title,
@@ -50,16 +48,16 @@ const NoteState = (props) =>{
 
     // Detele a Notes
     const deleteNote = (id) => {
-       // TODO: API Call 
-       console.log("deleting the note with id" + id)
-       const newNotes = notes.filter((note) => {return note._id !==id});
-       setNotes(newNotes)
+        // TODO: API Call 
+        console.log("deleting the note with id" + id)
+        const newNotes = notes.filter((note) => { return note._id !== id });
+        setNotes(newNotes)
     }
 
-        
-    return(
+
+    return (
         <>
-            <noteContext.Provider value={{notes, setNotes, addNote, editNote, deleteNote}}>
+            <noteContext.Provider value={{ notes, setNotes, addNote, editNote, deleteNote, getNotes }}>
                 {props.children}
             </noteContext.Provider>
         </>
